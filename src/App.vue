51dd8efb6bab26c608cd2d2e42173be7f07d4f2b
Matthias Jenny <matthiascjenny@gmail.com>
2018-09-02 16:37:02 -0400

    Change app name to Pumpkin Time

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PumpkinTime />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PumpkinTime from './components/PumpkinTime.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    PumpkinTime,
+  },
+};
 </script>
 
 <style>
