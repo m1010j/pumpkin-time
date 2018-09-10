be52e822850f57299515bc85a99bd5ea9d7d1189
Matthias Jenny <matthiascjenny@gmail.com>
2018-09-09 22:16:02 -0400

    Fix service worker

--- sw.js
+++ sw.js
@@ -1,15 +1,11 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('pumpkin-time').then(function(cache) {
-      console.log('caching...');
       return cache.addAll([
         './index.html',
-        './css/',
         './css/reset.css',
         './css/styles.css',
-        './dist/',
         './dist/main.js',
-        './fonts/',
         './fonts/SourceSansPro-Regular.ttf',
         './fonts/SourceSansPro-Bold.ttf',
       ]);
@@ -18,10 +14,17 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.open('pumpkin-time').then(function(cache) {
+      return cache.match(e.request).then(function(response) {
+        return (
+          response ||
+          fetch(e.request).then(function(response) {
+            cache.put(e.request, response.clone());
+            return response;
+          })
+        );
+      });
     })
   );
 });
